1f267fe65aa5f910c72155b5d2a711da81b087c9
Tim Bender <bendertj@gmail.com>
2025-01-25 10:46:08 -0500

    periodic timelines_dates.R

--- R/timelines_dates.R
+++ R/timelines_dates.R
@@ -15,34 +15,3 @@ renv::snapshot()
 cw_pos <- data.frame(pos_id   = c(nafill()), 
                      position = c(NA), 
                      agency   = c(NA))
-
-
-# x = 1:10
-# x
-# x[c(1:2, 5:6, 9:10)] = NA
-# x
-# nafill(x    = x, 
-#        type = "locf")
-# nafill(x    = x, 
-#        type = "nocb")
-# 
-# args(nafill)
-# 
-# dt = data.table(v1=x, v2=shift(x)/2, v3=shift(x, -1L)/2)
-# dt
-# nafill(dt, "nocb")
-# 
-# setnafill(dt, "locf", cols=c("v2","v3"))
-# dt
-# 
-# ?shift
-# 
-# x <- 1:5
-# x
-# shift(x, n = 1, fill = NA, type = "lag")
-# 
-# shift(x, n = 1:2, fill = 0, type = "lag")
-# 
-# shift(x, n = -1:1)
-# shift(x, n = -1:1, type = "shift", give.names = T)
-# shift(x, n = -1:1, type = "cyclic")
