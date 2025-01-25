869860057b45cdbed61ea1ea22ba2acc458fa30e
Tim Bender <bendertj@gmail.com>
2025-01-25 11:14:35 -0500

    timelines_dates.R perioidic

--- R/timelines_dates.R
+++ R/timelines_dates.R
@@ -15,11 +15,28 @@ rm(list=ls());cat('\f');gc()
 
 
 # build data----
-cw_pos <- data.table(pos_id        = factor(c("p01", "p02")), 
-                     position_name = c("School Board Member", "City Council Member"), 
-                     agency_name   = c("Durham Public Schools", "City of Durham"))
+cw_pos    <- data.table(pos_id        = (c("p01", "p02", "p03")), 
+                        position_name = c("School Board Member", 
+                                          "City Council Member", 
+                                          "County Commissioner"), 
+                        board_id      = c("b01", "b02", "b03"))
 
-cw_agency <- data.table(agency_id   = factor(c("a01", "a02")), 
-                        agency_name = c("Durham Public Schools", "City of Durham"), 
-                        agency_abbr = c("DPS"))
+cw_board <- data.table(board_id         = (c("b01", "b02", "b03")), 
+                       board_name       = c("Durham Public Schools Board of Education", 
+                                            "Durham City Council", 
+                                            "Durham County Commissioners"), 
+                       board_size       = c(7,7,5), 
+                       board_abbr       = c("DPS_boe", "DUR_cc", "DCO_bocc"),
+                       appointment_type = c("elected", "elected", "elected"), 
+                       term_months      = c(NA_integer_), 
+                       min_age          = c(NA, NA, 21))
 
+cw_member <- NULL
+
+for(i_p in cw_pos$pos_id){
+  print(i_p)
+}
+
+
+cw_pos
+cw_board
