dedfdb2df48fe3ae4e7659fce8572412998c48af
Tim Bender <bendertj@gmail.com>
2025-01-25 11:27:37 -0500

    building dataset

--- R/timelines_dates.R
+++ R/timelines_dates.R
@@ -31,12 +31,26 @@ cw_board <- data.table(board_id         = (c("b01", "b02", "b03")),
                        term_months      = c(NA_integer_), 
                        min_age          = c(NA, NA, 21))
 
+
+# generate member crosswalk table framework
 cw_member <- NULL
+m_id      <- 0 # for iterating through member ids below
 
 for(i_p in cw_pos$pos_id){
-  print(i_p)
+  for(i_m in 1:cw_board$board_size[cw_board$board_id == cw_pos$board_id[cw_pos$pos_id == i_p]]){
+    #print(i_m)
+    m_id <- m_id + 1
+    
+    cw_member <- rbind(cw_member, 
+                       data.frame(pos_id = i_p, 
+                                  #member_id = paste("m0", i_m, sep = "", collapse = "")))
+                                  member_id = paste("m", m_id, sep = "", collapse = ""), 
+                                  member_name = NA, 
+                                  ))
+  }
 }
 
+cw_member
 
 cw_pos
 cw_board
