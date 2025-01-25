461bc02738c90a44042e69756073f992ba2479d0
Tim Bender <bendertj@gmail.com>
2025-01-25 10:31:56 -0500

    renv updates

--- R/timelines_dates.R
+++ R/timelines_dates.R
@@ -1,6 +1,11 @@
 library(renv)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(janitor)
+# library(table)
 
-
-
+renv::status()
 renv::snapshot()
+
 # https://docs.google.com/document/d/1h0IvE_gZJcas2t_86wU52fi0b9Bf354UxSzy9bwsYuE/edit?tab=t.0
